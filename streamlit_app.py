cd6d12a56fccf032d4825f4f0581ed8d7da20953
Steven Adi Santoso <sstevenadi@gmail.com>
2024-01-30 19:06:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,21 @@ import requests
 import streamlit as st
 from openai import OpenAI
 
-# Fungsi untuk membuat elemen-elemen input
-def create_checkboxes(category, items):
+## Penggunaan fungsi create_checkboxes
+all_data = {}  # Untuk menyimpan semua data dari semua kategori
+
+def create_checkboxes(category_label, items):
     saved_data = {}  # Untuk menyimpan data
     
     for index, item in enumerate(items):
-        unique_key = f"{category}{item['name']}{item['label']}_{index}"
+        unique_key = f"{category_label}{item['name']}{item['label']}_{index}"
         if item["type"] == "checkbox":
             saved_data[unique_key] = st.checkbox(item["label"], key=unique_key)
         elif item["type"] == "text":
             saved_data[unique_key] = st.text_input("", key=unique_key, placeholder="Other (please specify)")
-    
-    if st.button("Simpan"):  # Tombol untuk menyimpan data
-        # Lakukan sesuatu dengan data yang disimpan, misalnya kirim ke database
-        st.write("Data yang disimpan:", saved_data)
 
-# Contoh penggunaan dengan data dari file JSON
-for category in data:
-    st.header(category["title"])
-    create_checkboxes(category["label"], category["answer"])
+    all_data.update(saved_data)  # Update the data dictionary
+    return saved_data
 
 def main():
     st.set_page_config(page_title="Person Data Creator")
@@ -102,17 +98,17 @@ def main():
     with routines_tab:
         for category in data[0]["routines"]:
             st.subheader(category["title"])
-            create_checkboxes(category["label"], category["answer"])
+            category_data = create_checkboxes(category["label"], category["answer"])
 
     with likes_tab:
         for category in data[1]["likes"]:
             st.subheader(category["title"])
-            create_checkboxes(category["label"], category["answer"])
+            category_data = create_checkboxes(category["label"], category["answer"])
 
     with dislikes_tab:
         for category in data[2]["dislikes"]:
             st.subheader(category["title"])
-            create_checkboxes(category["label"], category["answer"])
+            category_data = create_checkboxes(category["label"], category["answer"])
 
 
     # Person tab
